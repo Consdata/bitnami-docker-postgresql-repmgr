6c6381eedb056896bd94edaaf3ef2d359796e53d
Jakub Goszczurny <4983958+szczurmys@users.noreply.github.com>
2020-03-19 10:48:08 +0100

    postgres conf no such file local varsion (#5)
    * Fixed error "/bitnami/postgresql/data/postgresql.conf": No such file or directory - it appears when we have more than 2 nodes and the primary one died.
    
    * Added pg_ctl_options also in new profile

--- 9.6/debian-10/rootfs/librepmgr.sh
+++ 9.6/debian-10/rootfs/librepmgr.sh
@@ -491,6 +491,7 @@ log_level='${REPMGR_LOG_LEVEL}'
 priority='${REPMGR_NODE_PRIORITY}'
 degraded_monitoring_timeout='${REPMGR_DEGRADED_MONITORING_TIMEOUT}'
 data_directory='${POSTGRESQL_DATA_DIR}'
+pg_ctl_options='-l $POSTGRESQL_LOG_FILE -o --config-file="$POSTGRESQL_CONF_FILE --external_pid_file=$POSTGRESQL_PID_FILE --hba_file=$POSTGRESQL_PGHBA_FILE"'
 async_query_timeout='${REPMGR_MASTER_RESPONSE_TIMEOUT}'
 EOF
     fi
